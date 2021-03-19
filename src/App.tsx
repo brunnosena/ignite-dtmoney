70408b0a73ecdbf49d4b7830b74e19aea0ac573b
Brunno Sena <brunnow@hotmail.com>
2021-03-19 14:22:23 -0300

    classes finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Modal from "react-modal";
 
-function App() {
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashoard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+import { TransactionsProvider } from "./contexts/TransactionsContext";
+import { ModalProvider } from "./contexts/ModalContext";
+
+import { GlobalStyle } from "./styles/global";
+Modal.setAppElement("#root");
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TransactionsProvider>
+      <ModalProvider>
+        <GlobalStyle />
+
+        <Header />
+
+        <Dashboard />
+
+        <NewTransactionModal />
+      </ModalProvider>
+    </TransactionsProvider>
   );
 }
-
-export default App;
